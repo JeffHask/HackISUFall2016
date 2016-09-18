8d6516e9c03518565fbc6c2abe3804ae4cdb9a44
Jeffrey Haskovec <jeffhaskovec@gmail.com>
2016-09-18 05:39:30 -0500

    Merge branch 'master' of https://github.com/JeffHask/HackISUFall2016
    # Conflicts:
    #	js/container/CaptionContainer.js

--- js/container/CaptionContainer.js
+++ js/container/CaptionContainer.js
@@ -157,15 +157,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#EEEEEE'
   },
   image: {
-    width: 400,
-    height: 400,
+    width: 350,
+    height: 350,
     marginTop: 5,
     alignItems: 'center',
-    marginBottom: 50
+    marginBottom: 30
   },
   backdropViewTop: {
-    height: 400,
-    width: 400,
+    height: 350,
+    width: 350,
     backgroundColor: 'rgba(0,0,0,0)',
     flexDirection: 'column',
     justifyContent: 'space-between'
